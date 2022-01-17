f5a756bf1d955b4a5c4dd458ffb66a8d48232c0d
Lara Lays <lara.lays.bh6@gmail.com>
2022-01-17 18:16:05 -0300

    Criando rotas com express

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const FilmesServices = require('./services/FilmesServices');
+// Importação da biblioteca Express
+const express = require('express');
 
-console.log(FilmesServices.listar());
+// Criando um servidor(app)
+const servidor = express();
+
+// Criando as rotas do servidor
+servidor.get('/', (req,res) => {
+    console.log("Alguem fez uma requisição...")
+    res.send('Você está na página inicial!')
+});
+
+servidor.get('/filmes', (req,res) => {
+    console.log("Processando a requisição...")
+    res.send('A página filmes está aberta!')
+});
+
+servidor.get('/generos', (req,res) =>{
+    res.send('Página generos tá on!')
+})
+// Por o servidor para 'ouvir' as requisições
+servidor.listen(3000)
