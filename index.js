f1710f268677628cc4eee1c0f8a036f5802b8da1
Lara Lays <lara.lays.bh6@gmail.com>
2022-01-20 17:15:51 -0300

    Criando uma rota dinamica

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Importação da biblioteca Express
 const express = require('express');
+const res = require('express/lib/response');
 
 // Criando um servidor(app)
 const servidor = express();
@@ -17,6 +18,16 @@ servidor.get('/filmes', (req,res) => {
 
 servidor.get('/generos', (req,res) =>{
     res.send('Página generos tá on!')
+});
+
+servidor.get('/filmes/:posicao', (req,res) =>{
+    let posicao = req.params.posicao;
+    const filmes = require('./database/filmes.json');
+    res.send(filmes[posicao])
+});
+
+servidor.get('/busca/venom:posicao', (req,res) =>{
+
 })
 // Por o servidor para 'ouvir' as requisições
 servidor.listen(3000)
